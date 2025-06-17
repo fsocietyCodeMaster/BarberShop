731e2684270d81d6bcc644e51e77758406f98162
pouya000 <puya.shamsi@gmail.com>
2025-06-17 13:49:27 +0330

    appronal first step ...

--- BarberShop/ionicUi/BookItNow/src/app/tabs-barbershop/tab8/tab8.component.ts
+++ BarberShop/ionicUi/BookItNow/src/app/tabs-barbershop/tab8/tab8.component.ts
@@ -4,6 +4,7 @@ import { IonicModule } from '@ionic/angular';
 import { UserService } from '../../services/user.service';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ToastController } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-tab8',
@@ -20,46 +21,96 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 export class Tab8Component implements OnInit {
 
   constructor(private userservice: UserService,
+    private toastCtrl: ToastController,
     private router: Router) {
 
   }
 
   barbers_list: any[] = [];
 
-  barbershopId: any; 
+  barbershopId: any;
 
 
   ngOnInit() {
     this.userservice.getUserInfo().subscribe((data: any) => {
       this.barbershopId = data.data['id'];
-      console.log('this.barbershopId: ', this.barbershopId )
+      console.log('this.barbershopId: ', this.barbershopId)
     })
   }
 
   showTable = false;
 
- 
+
 
   toggleTable() {
     this.showTable = !this.showTable;
   }
 
   confirm(barber: any) {
-    console.log('تأیید شد:', barber.fullName);
+
+    console.log('barberId in confirm methide:', barber);
+
+
+    const data = {
+      "userId": barber.id,
+      "approval": "verify"
+    };
+
+    this.userservice.barberApproval(data).subscribe(async (res: any) => {
+
+      console.log('data of approve ini API:', data);
+
+      if (res.isSuccess) {
+        const t = await this.toastCtrl.create({ message: `${barber.fullName} با موفقیت تایید شد `, duration: 2000, color: 'success' });
+        await t.present();
+      } else {
+        const t = await this.toastCtrl.create({ message: 'خطا در بروزرسانی', duration: 2000, color: 'danger' });
+        await t.present();
+      }
+    });
+
   }
 
   reject(barber: any) {
-    console.log('رد شد:', barber.fullName);
+
+    console.log('barberId in rejct methide:', barber);
+
+    const data = {
+      "userId": barber.id,
+      "approval": "reject"
+    };
+
+    this.userservice.barberApproval(data).subscribe(async(res: any) => {
+
+      console.log('data of reject in API:', data);
+
+      if (res.isSuccess) {
+        const t = await this.toastCtrl.create({ message: `${barber.fullName} غیر فعال `, duration: 2000, color: 'success' });
+        await t.present();
+      } else {
+        const t = await this.toastCtrl.create({ message: 'خطا در بروزرسانی', duration: 2000, color: 'danger' });
+        await t.present();
+      }
+
+    });
+
   }
 
   barberList() {
     this.userservice.Getbarberbybarbershop().subscribe((data: any) => {
+
       console.log("data is: ", data);
+
       //this.barbers_list = data.data['barbers'];
+
       this.barbers_list = data.data;
+
       console.log("barbers_listis: ", this.barbers_list, typeof (this.barbers_list));
+
+      //this.barbers_list.filter((barber) => barber.status == 1);
+
       this.toggleTable();
     })
   }
-  
+
 }
