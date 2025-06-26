bdf3f60ade3c5720d8cf020af6a4c362c292475b
pouya000 <puya.shamsi@gmail.com>
2025-06-26 17:06:30 +0330

    for improve conflict

---
+++ BarberShop/Migrations/20250624050511_request_barbershop_Id.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BarberShop.Migrations
+{
+    /// <inheritdoc />
+    public partial class request_barbershop_Id : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "RequestedBarberShopId",
+                table: "AspNetUsers",
+                type: "uniqueidentifier",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RequestedBarberShopId",
+                table: "AspNetUsers");
+        }
+    }
+}
